3f7127eeb9aade982d877ab69a6a53ed695aa652
jlcomar <131366934+jlcomar@users.noreply.github.com>
2023-08-01 08:37:49 -0300

    Delete bootstrap Serenatto directory

--- bootstrap Serenatto/script.js
+++
@@ -1,7 +0,0 @@
-const inputCheck = document.querySelector('#modo-noturno')
-const elemento = document.querySelector('body')
-
-inputCheck.addEventListener('click', () => {
-    const modo = inputCheck.checked ? 'dark' : 'light'
-    elemento.setAttribute("data-bs-theme", modo)
-})
