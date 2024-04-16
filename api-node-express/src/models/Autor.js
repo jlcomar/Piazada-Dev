d9a43318955e872c1a5d658d5a26efd29bed0fb3
jlcomar <131366934+jlcomar@users.noreply.github.com>
2024-04-16 15:04:15 -0300

    Add files via upload

---
+++ api-node-express/src/models/Autor.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const autorSchema = new mongoose.Schema(
+  {
+    id: {type: String},
+    nome: {
+      type: String,
+      required: [true, "O nome do(a) autor(a) é obrigatório"]
+    },
+    nacionalidade: {type: String}
+  },
+  {
+    versionKey: false
+  }
+);
+
+const autores = mongoose.model("autores", autorSchema);
+
+export default autores;
